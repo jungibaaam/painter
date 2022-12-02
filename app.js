c3f5fe0a909ce2153956a11063910e0c8cc82fc0
jungi <joongi1217@gmail.com>
2022-12-02 17:25:26 +0900

    LineWidth

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 function onMove(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.fill();
+        ctx.stroke();
         return;
     }
     ctx.moveTo(event.offsetX, event.offsetY);
@@ -17,9 +18,16 @@ function onMouseDown() {
 }
 function canclePainting() {
     isPainting = false;
+    ctx.beginPath();
+
+}
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", canclePainting);
 canvas.addEventListener("mouseleave", canclePainting)
+
+lineWidth.addEventListener("change", onLineWidthChange);
