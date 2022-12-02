db8284e8bdb7000ef8efb9f72e801070ab079669
jungi <joongi1217@gmail.com>
2022-12-02 16:57:34 +0900

    drawUntilMouseClick

--- app.js
+++ app.js
@@ -2,27 +2,24 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff",
-    "#7d5fff",
-]
 ctx.lineWidth = 2;
-function onClick(event) {
-    ctx.beginPath();
-    console.log(event);
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+let isPainting = false;
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.fill();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function onMouseDown() {
+    isPainting = true;
+}
+function canclePainting() {
+    isPainting = false;
 }
 
-canvas.addEventListener("mousemove", onClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", canclePainting);
+canvas.addEventListener("mouseleave", canclePainting)
