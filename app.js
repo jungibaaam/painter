3f776a89438f9b323e049e6264677cff96205366
jungi <joongi1217@gmail.com>
2022-12-02 20:18:46 +0900

    ImageInput

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserbtn = document.getElementById("eraser-btn");
@@ -66,6 +67,16 @@ function onEraserClick() {
     isFilling = false;
     modeBtn.innerText = "Fill";
 }
+function onFileChange(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function(){
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
@@ -80,3 +91,4 @@ colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserbtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
